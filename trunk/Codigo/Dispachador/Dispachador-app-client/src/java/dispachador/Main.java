5aaae8ace549d592722d5319bfafc9f80c4198d8
rsantellan <rsantellan@d1d28f38-517b-11de-b1d8-e9bd45c23c72>
2009-06-16 03:17:10 +0000

    Ultima version

--- trunk/Codigo/Dispachador/Dispachador-app-client/src/java/dispachador/Main.java
+++ trunk/Codigo/Dispachador/Dispachador-app-client/src/java/dispachador/Main.java
@@ -5,17 +5,35 @@
 
 package dispachador;
 
+import javax.ejb.EJB;
+import uy.edu.ort.arq.obligatorio.dominio.Servicios;
+import uy.edu.ort.arq.obligatorio.popularbase.PopularBaseRemote;
+
 /**
  *
  * @author rodrigo
  */
 public class Main {
 
+    @EJB
+    private static PopularBaseRemote pop;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Servicios s = new Servicios();
+        s.setNombreEmpresa("Weather");
+        s.setNombreServicio("temperaturaFarenheitToCelsius");
+        s.setUri("lala");
+        s.setActivo(true);
+        pop.popular(s);
+        s.setNombreEmpresa("Weather");
+        s.setNombreServicio("temperaturaCelsiusToFarenheit");
+        s.setUri("lala");
+        s.setActivo(true);
+        pop.popular(s);
     }
 
 }
